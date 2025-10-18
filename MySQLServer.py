59820d6ae94fa7a8eb480bde2d724f21c292a878
Sifiso Ntuli <sifisontulisn88@gmail.com>
2025-10-18 10:47:10 +0200

    new commit

--- MySQLServer.py
+++ MySQLServer.py
@@ -17,7 +17,7 @@ def create_db():
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_sql_db' created successfully.")
 
-    except Error as e:
+    except mysql.connector.Error as e:
         print(f"Error while connecting to MySQL: {e}")
 
     finally:
