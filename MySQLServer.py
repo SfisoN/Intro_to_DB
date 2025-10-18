b903ee30a5a9cf41dfd5e494f21eafa30b30361e
Sifiso Ntuli <sifisontulisn88@gmail.com>
2025-10-18 10:31:24 +0200

    new project commit

---
+++ MySQLServer.py
@@ -0,0 +1,31 @@
+import mysql.connector
+from mysql.connector import Error
+
+
+def create_db():
+    """Create a new database named 'alx_sql_db'."""
+    try:
+        # Connect to MySQL server
+        connection = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password='EMi6gR7@US!qrjA'
+        )
+
+        if connection.is_connected():
+            cursor = connection.cursor()
+            cursor.execute("CREATE DATABASE IF NOT EXISTS alx_sql_db")
+            print("Database 'alx_sql_db' created successfully.")
+
+    except Error as e:
+        print(f"Error while connecting to MySQL: {e}")
+
+    finally:
+      if 'cursor' in locals() and cursor is not None:
+          cursor.close()
+      if connection in locals() and connection.is_connected():
+          connection.close()
+
+
+if __name__ == "__main__":
+    create_db()
