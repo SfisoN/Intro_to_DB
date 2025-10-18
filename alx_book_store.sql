2effe679b68961ae7fc5429315f6c676f302d8c2
Sifiso Ntuli <sifisontulisn88@gmail.com>
2025-10-18 10:15:46 +0200

    update

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -39,8 +39,8 @@ CREATE TABLE `Authors` (
 
 CREATE TABLE `Customers` (
   `customer_id ` INT NOT NULL AUTO_INCREMENT,
-  `customer_name ` VARCHAR(215) NOT NULL,
-  `email` VARCHAR(215) NOT NULL,
+  `customer_name ` VARCHAR(215),
+  `email` VARCHAR(215),
   `address` TEXT,
   PRIMARY KEY (`customer_id`),
     UNIQUE INDEX `idx_email` (`email`)
