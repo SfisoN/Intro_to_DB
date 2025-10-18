718787a6c51dcd3c4edbf4347b74ee941bc0a090
Sifiso Ntuli <sifisontulisn88@gmail.com>
2025-10-18 11:17:12 +0200

    update

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -56,7 +56,7 @@ CREATE TABLE `Orders` (
         INDEX `idx_orders_customer_id` (customer_id),
         CONSTRAINT `fk_orders_customer`
             FOREIGN KEY (customer_id)
-            REFERENCES Customers (customer_id)
+            REFERENCES Customers(customer_id)
             ON DELETE CASCADE
             ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
